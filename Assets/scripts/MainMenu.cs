29ee91cfcea1e7e91d565d831b6968c9be6beed9
Vik Goel <vikash.a.goel@gmail.com>
2015-10-18 14:21:05 -0400

    added a play button

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera cam = Camera.main;
+		transform.localScale = new Vector3(cam.orthographicSize/2 * (Screen.width/Screen.height),1f, cam.orthographicSize/2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
